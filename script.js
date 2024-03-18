820cddc7bc1f2bae9f5f28f347e320726f95598b
r-vaughn <rebecca.a.vaughn@outlook.com>
2024-03-18 00:17:38 -0500

    pickPlanet, myFetch, and addDestination functions working; 10 of 10 tests passing

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Write your JavaScript code here!
 
-//const { formSubmission } = require("./scriptHelper");
+
 
 window.addEventListener("load", function() {
     let form = document.querySelector("form");
@@ -11,27 +11,22 @@ window.addEventListener("load", function() {
         const fuelLevel = document.querySelector("input[name=fuelLevel]");
         const cargoMass = document.querySelector("input[name=cargoMass]");
         const list = document.getElementById("faultyItems");
-            if ( formSubmission(document, list, pilotName.value, copilotName.value, fuelLevel.value, cargoMass.value) === "pass") {
-                document.getElementById("launchStatus").innerHTML = "Shuttle is Ready for Launch";
-                document.getElementById("launchStatus").style.color = "green"; 
-            } 
-               
-            
-            
-            
-            //document.getElementById("pilotStatus").innerHTML = `${pilotName.value}`; 
-        
-
+            formSubmission(document, list, pilotName.value, copilotName.value, fuelLevel.value, cargoMass.value);    
     });
+
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
+    let listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
         console.log(listedPlanets);
     }).then(function () {
         console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        let randomPlanet = pickPlanet(listedPlanets);
+        console.log(randomPlanet);
+        addDestinationInfo(document, randomPlanet.name, randomPlanet.diameter, randomPlanet.star, randomPlanet.distance, randomPlanet.moons, randomPlanet.image);
+        
     })
     
  });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,20 +1,22 @@
 // // Write your helper functions here!
 
-//require('cross-fetch/polyfill');
+require('cross-fetch/polyfill');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
+    
+    document.getElementById("missionTarget").innerHTML =
     // Here is the HTML formatting for our mission target div.
-    /*
-                 <h2>Mission Destination</h2>
+    
+                 `<h2>Mission Destination</h2>
                  <ol>
-                     <li>Name: </li>
-                     <li>Diameter: </li>
+                     <li>Name: ${name} </li>
+                     <li>Diameter: ${diameter}</li>
                      <li>Star: ${star}</li>
-                     <li>Distance from Earth: </li>
-                     <li>Number of Moons: </li>
+                     <li>Distance from Earth: ${distance}</li>
+                     <li>Number of Moons: ${moons}</li>
                  </ol>
-                 <img src="">
-    */
+                 <img src="${imageUrl}">`
+
  }
      
         
@@ -101,14 +103,17 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
  async function myFetch() {
      let planetsReturned;
  
-     planetsReturned = await fetch().then( function(response) {
-         });
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+        return response.json();
+    });
  
      return planetsReturned;
  }
  
- function pickPlanet(planets) {
- }
+function pickPlanet(planets) {         //planets = json array of planets
+  let index = Math.floor(Math.random()*planets.length);
+  return planets[index];                //should return json[index]
+}
  
  module.exports.addDestinationInfo = addDestinationInfo;
  module.exports.validateInput = validateInput;
